5919df62319bf6b9503d188800d2857a3062f1b9
Candice Peters <candicelizabethpeters@gmail.com>
2019-08-20 23:59:19 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,14 +1,13 @@
-class Dog 
-  def name= (dog_name)
-    @this_dog_name = dog_name
+class Dog
+ 
+  def name=(dogs_name)
+    @this_dogs_name = dogs_name
   end
-  
-  def name 
-    @this_dog_name
+ 
+  def name
+    @this_dogs_name
   end
-  end
-
+end
+ 
 lassie = Dog.new
 lassie.name = "Lassie"
- 
- lassie.name
